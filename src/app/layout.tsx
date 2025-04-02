72ed443ce9e78b065e1e1f94ee32e1bdc281faa2
Divyesh Parmar <dp032040@gmail.com>
2025-04-02 14:30:14 +0530

    remove comments

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,33 +1,4 @@
 
-
-// import { Toaster } from "@/components/ui/toaster"
-// import AuthProvider from "@/context/AuthProviders";
-// import '../globals.css'
-
-// export const metadata = {
-//   title: 'True Feedback',
-//   description: 'Build by Next.js',
-// }
-
-// export default function RootLayout({
-//   children,
-// }: {
-//   children: React.ReactNode
-// }) {
-//   return (
-//     <AuthProvider>
-//         <html lang="en">
-//             <body>
-                
-//                 {children}  
-//                 <Toaster /> 
-//             </body>
-//         </html>
-//     </AuthProvider>   
-//   )
-// }
-
-
 import { Toaster } from "@/components/ui/toaster";
 import AuthProvider from "@/context/AuthProviders";
 import "../globals.css";
